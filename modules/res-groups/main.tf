681fd43a1901b4b42d6eaf21f43dac5ded5a0bdd
Lenny Capellan <104685438+git719@users.noreply.github.com>
2024-03-09 12:18:06 -0500

    drop prefix in name

--- modules/res-groups/main.tf
+++ modules/res-groups/main.tf
@@ -1,7 +1,6 @@
 
 locals {
   res_groups_updated = [for i, rg in var.res_groups : {
-    name_prefix = rg.name_prefix
     name        = rg.name
     location    = rg.location != null && rg.location != "" ? rg.location : "eastus"
     tags        = rg.tags != null ? rg.tags : {}
@@ -10,7 +9,7 @@ locals {
 
 resource "azurerm_resource_group" "rg" {
   count    = length(local.res_groups_updated)
-  name     = "${local.res_groups_updated[count.index].name_prefix}-${local.res_groups_updated[count.index].name}"
+  name     = "${local.res_groups_updated[count.index].name}"
   location = local.res_groups_updated[count.index].location
   tags     = local.res_groups_updated[count.index].tags
 }
--- modules/res-groups/variables.tf
+++ modules/res-groups/variables.tf
@@ -2,7 +2,6 @@
 variable "res_groups" {
   description = "List of Azure resource groups to be managed."
   type = list(object({
-    name_prefix = string
     name        = string
     location    = optional(string)
     tags        = optional(map(string))
